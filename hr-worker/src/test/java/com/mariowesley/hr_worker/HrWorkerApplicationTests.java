040076a1ea8d34d3be405af231b9ce6b52c4fad9
WMBarbosa <wesleymario01@gmail.com>
2024-07-29 06:30:56 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/mariowesley/hr_worker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariowesley.hr_worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
